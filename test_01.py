974cb6c8a048328995fe5b9d8fa43c4e3b466e34
stefanonae <sbruzzo@uchicago.edu>
2022-03-31 09:34:03 -0500

    Create test_01.py
    This was the first lines of code for the testing repo

---
+++ test_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 09:32:12 2022
+
+@author: stefanobruzzo
+"""
+
+# testing the repo I just created on GitHub
+
+
+print("Hello world!")
+
+print("This is a test")
