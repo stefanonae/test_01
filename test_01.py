fa1ed9bf14606da251b59e5ea6e4e0e3e773a458
stefanonae <sbruzzo@uchicago.edu>
2022-03-31 09:46:33 -0500

    Update test_01.py
    new iteration of the testing code

--- test_01.py
+++ test_01.py
@@ -9,6 +9,12 @@ Created on Thu Mar 31 09:32:12 2022
 # testing the repo I just created on GitHub
 
 
-print("Hello world!")
+hw = "Hello world!"
 
 print("This is a test")
+
+name = "Stefano Bruzzo-Gallardo"
+
+print (name)
+
+print(hw,"My name is",name)
